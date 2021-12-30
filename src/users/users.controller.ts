f37a36bb3aef0c7f23708474c46c957b1f41f5e7
LeoScripts <leandroguitarjesus89@gmail.com>
2021-12-30 15:56:03 -0300

    determinando que sometes administradores criem adiministradores

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UsersService } from './users.service';
-import { ReturnUserDto } from './dto/return-user.dto';
-
-@Controller('users')
-export class UsersController {
-  constructor(private usersService: UsersService) {}
-
-  @Post()
-  async createAdminUser(
-    @Body(ValidationPipe) createUserDto: CreateUserDto,
-  ): Promise<ReturnUserDto> {
-    const user = await this.usersService.createAdminUser(createUserDto);
-    return {
-      user,
-      message: 'Administrador cadastrado com sucesso',
-    };
+import {
+    Controller,
+    Post,
+    Body,
+    ValidationPipe,
+    UseGuards,
+  } from '@nestjs/common';
+  import { CreateUserDto } from './dto/create-user.dto';
+  import { UsersService } from './users.service';
+  import { ReturnUserDto } from './dto/return-user.dto';
+  import { AuthGuard } from '@nestjs/passport';
+  
+  @Controller('users')
+  export class UsersController {
+    constructor(private usersService: UsersService) {}
+  
+    @Post()
+    @UseGuards(AuthGuard())
+    async createAdminUser(
+      @Body(ValidationPipe) createUserDto: CreateUserDto,
+    ): Promise<ReturnUserDto> {
+      const user = await this.usersService.createAdminUser(createUserDto);
+      return {
+        user,
+        message: 'Administrador cadastrado com sucesso',
+      };
+    }
   }
-}
