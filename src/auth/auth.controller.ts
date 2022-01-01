12cb01fd8a700a62fc1ecf60802cd843d4e47efa
LeoScripts <leandroguitarjesus89@gmail.com>
2022-01-01 17:48:38 -0300

    email de confirmaçao e solicitaçao de troca de senha

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,44 +1,62 @@
 import {
-    Controller,
-    Post,
-    Body,
-    ValidationPipe,
-    Get,
-    UseGuards,
-    Req,
-  } from '@nestjs/common';
-  import { AuthService } from './auth.service';
-  import { CreateUserDto } from '../users/dto/create-user.dto';
-  import { CredentialsDto } from '../users/dto/credentials.dto';
-  import { AuthGuard } from '@nestjs/passport';
-  import { User } from '../users/user.entity';
-  import { GetUser } from './get-user.decorator';
-  
-  @Controller('auth')
-  export class AuthController {
-    constructor(private authService: AuthService) {}
-  
-    @Post('/signup')
-    async signUp(
-      @Body(ValidationPipe) createUserDto: CreateUserDto,
-    ): Promise<{ message: string }> {
-      await this.authService.signUp(createUserDto);
-      return {
-        message: 'Cadastro realizado com sucesso',
-      };
-    }
-  
-    @Post('/signin')
-    async signIn(
-      @Body(ValidationPipe) credentiaslsDto: CredentialsDto,
-    ): Promise<{ token: string }> {
-      return await this.authService.signIn(credentiaslsDto);
-    }
-  
-    @Get('/me')
-    @UseGuards(AuthGuard())
-    getMe(@GetUser() user: User): User {
-      return user;
-    }
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Get,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { CredentialsDto } from '../users/dto/credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../users/user.entity';
+import { GetUser } from './get-user.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<{ message: string }> {
+    await this.authService.signUp(createUserDto);
+    return {
+      message: 'Cadastro realizado com sucesso',
+    };
   }
-  
+
+  @Post('/signin')
+  async signIn(
+    @Body(ValidationPipe) credentiaslsDto: CredentialsDto,
+  ): Promise<{ token: string }> {
+    return await this.authService.signIn(credentiaslsDto);
+  }
+
+  @Patch(':token')
+  async confirmEmail(@Param('token') token: string) {
+    await this.authService.confirmEmail(token);
+    return {
+      message: 'Email confirmado',
+    };
+  }
+
+  @Post('/send-recover-email')
+  async sendRecoverPasswordEmail(
+    @Body('email') email: string,
+  ): Promise<{ message: string }> {
+    await this.authService.sendRecoverPasswordEmail(email);
+    return {
+      message: 'Foi enviado um email com instruções para resetar sua senha',
+    };
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  getMe(@GetUser() user: User): User {
+    return user;
+  }
+}
---
+++ src/configs/mailer.config.ts
@@ -0,0 +1,16 @@
+import { MailerOptions } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as path from 'path';
+
+export const mailerConfig: MailerOptions = {
+  template: {
+    dir: path.resolve(__dirname, '..', '..', 'templates'),
+    adapter: new HandlebarsAdapter(),
+    options: {
+      extName: '.hbs',
+      layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+    },
+  },
+//   dados do seu email como login e senha
+  transport: `smtps://leandroguitarjesus89@gmail.com:jljslmabr88@smtp.gmail.com`,
+};
